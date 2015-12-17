e2b98999f910c096476dbce2e4c2d8fe8e5fe4d8
Matt Watier <mattwatier@gmail.com>
2015-12-17 09:20:29 -0500

    Foundation 5 branch
    Removing Foundation 5 Branch from Main Build so Hot Fixes Can be dealt with.

---
+++ GruntFile.js
@@ -0,0 +1,191 @@
+module.exports = function(grunt){
+	//Project Config
+	grunt.initConfig({
+		 connect:{
+      server:{
+        options:{
+			livereload:true,
+          	port: 8000,
+          	open:{
+            target: 'http://localhost:8000', // target url to open
+          },
+        //keepalive:true,
+        }
+      }
+    },
+    		cssmin: {
+			  options: {
+			    shorthandCompacting: false,
+			    roundingPrecision: -1
+			  },
+			  target: {
+			    files: [{
+			      expand: true,
+			      cwd: 'assets/css',
+			      src: ['*.css', '!*.min.css'],
+			      dest: 'assets/css',
+			      ext: '.min.css'
+			    }]
+			  }
+			},	
+		liquid: {
+			options: {
+			  includes: 'templates/includes'
+			},
+			pages: {
+			  files: [
+			    { expand: true, flatten: true, src: 'templates/*.liquid', dest: 'converted-html/', ext: '.html' }
+			  ]
+			}
+		},
+		sass:{
+			dist:{			
+				files:[{
+					expand: true,
+					cwd: './assets/scss',
+					src: ['*.scss'],
+					dest: './assets/css',
+					ext: '.css'	
+				}]
+			},
+			styleguide:{			
+				files:[{
+					expand: true,
+					cwd: './assets/scss',
+					src: ['*.scss'],
+					dest: './styleguide/assets/css',
+					ext: '.css'	
+				}]
+			},
+			foundation:{
+				files:[{
+					expand: true,
+					cwd: './assets/scss/vendor/foundation',
+					src: ['*.scss','_foundation-override/*.scss'],
+					dest: './assets/css',
+					ext: '.css'	
+			}]
+		}
+
+		},
+		styledown: {
+		    base: {
+		      files: {
+		       	'styleguide/index.html': 'assets/css/index.css',
+		      },
+		      options: {
+		        css: [ "assets/css/normalize.min.css",'http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,700,600,400,300|Roboto+Slab:400,700|Raleway:300','assets/css/jquery-ui.css','assets/css/jquery-ui.structure.css',"assets/css/foundation.min.css",'assets/css/index.css'],
+		        js: ['assets/js/jquery-2.1.3.min.js','assets/js/selectonic.min.js','assets/js/chosen/chosen.jquery.min.js','assets/js/jquery-ui.js','assets/js/foundation.min.js','assets/js/knockout.js','assets/js/app.js'],
+		        title: 'My Style Guide',
+		        sg_css: 'assets/css/styledown.css',
+		        sg_js: 'assets/js//styledown.js',
+		        //config: 'assets/css/config.md'
+		      }
+		    }
+
+
+		  },
+		copy:{ 
+			STYLES: {
+				files: [{
+					expand: true,
+					cwd: 'styleguide/',
+					src: '**',
+					dest: '//rdstgweb5/stage.tools.acc.org/BaseStyle'
+				},
+				{
+					expand: true,
+					cwd: 'styleguide/',
+					src: '**',
+					dest: '//rdstgweb6/stage.tools.acc.org/BaseStyle'
+				}]
+			},
+			INTERNAL:{
+				files:[
+				{
+					expand:true,
+					cwd:'assets/js',
+					src:'**',
+					dest:'styleguide/assets/js',
+				}
+				,{
+					expand:true,
+					cwd:'assets/css',
+					src:'index.*',
+					dest:'styleguide/assets/css',
+				},{
+					expand:true,
+					cwd:'assets/img',
+					src:'**',
+					dest:'styleguide/assets/img',
+				},{
+					expand:true,
+					cwd:'assets/js',
+					src:'**',
+					dest:'converted-html/assets/js',
+				}
+				,{
+					expand:true,
+					cwd:'assets/css',
+					src:['index.*','foundaiton.*','font-awesome.*','jquery-ui','styledown.*'],
+					dest:'converted-html/assets/css',
+				},{
+					expand:true,
+					cwd:'assets/img',
+					src:'**',
+					dest:'converted-html/assets/img',
+				}
+				]
+			}
+
+		},
+	    watch: {
+	    	options: {
+			      livereload: true,
+			    },
+			js: {
+			files: [ 'assets/js/*.js','templates/includes/scripts.liquid' ],
+			tasks: [ 'liquid','copy:INTERNAL' ],
+
+			},
+			data: {
+			files: [ 'templates/**' ],
+			tasks: [ 'liquid'],
+
+			},
+			scss: {
+			files: [ 'assets/scss/*.scss','assets/scss/*/*.scss','assets/scss/*/*/*.scss' ],
+			tasks: [ 'sass:dist','cssmin','styledown:base','copy:INTERNAL'],
+
+			}
+	    },
+		clean: {
+			localFolder:["converted-html/**"],
+			sassCasheFolder:[".sass-cache/**"]
+
+		},
+	});
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-copy');	
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-liquid');
+	grunt.loadNpmTasks('grunt-styledown');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-sass-convert');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	// Default task.
+	grunt.registerTask('build', [
+		'connect',
+		'sass:foundation',
+		'sass:dist',
+		'cssmin',
+		'copy:INTERNAL',
+		'styledown',
+		'watch',
+		
+	]);
+	grunt.registerTask('serve', [
+		'connect',]);
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+Welcome ACC Base APP UI Ver 2.0
+-------------------
+This is a library of modular styles powered by the framework of Zurb Foundation. This design was implemented for ACC Apps Construction. It is responsive and can be scaled up and down for the needs of the UI that is needed. Things to note this library is built with Sass to give the greatest flexablity for ACC App Builds. 
+
+## Concept
+This library is built with some exposed modifiers and a collection of UI designed components that can be included or not included. To reduce inproper cascades in CSS the Sass inlcudes and folder structure has been broken into these layers
+
+1. __base__ - any file that deals with varables, fonts and basic typography (raw tags).
+2. __helpers__ or 'required.scss ' - Mostly mixens but some files that have "required" in the name are needed for other files to functions. any file that have been named with "trump" will
+3. __components__ - any combination of DOM elements that make up a widget, nav, or collection of tags.
+4. __vendor__ - Any included style from a javascript or framework that needs an override to be inline with ACC standards.
+5. __structure__ - Non-Styling Pure Page Layout Code.  Balanced Columns, Sidebars, Page Spacing etc...
+6. __views__ - Overrides of components when in specific cases of the page or theme they are in. Like Sidebar Box on the Home page would be "_home.sidebarbox.scss"
+7. __trumps__ or '.trump.scss' - utility classes that have either high specificty or should override other uses of the same style in earlier stylesheets.  
+
+As you travel down the structure of the main document the included stylesheets should increase there specificty. 
+
+
+### Expansion
+Most alteration to the code can be done through "non-core" inclusion of styles. Please use the mirrored folder structure to allow of review and understanding of what level of specificty should be used in the document. 
+
+#### Product Color and Branding
+In the main style sheet there are some exposed variables to alter the skin of the style.  One of th most cosmetic is product color and branding. The variable "$product-brand-color" takes a hex color and will change elements of the ui to this color away from the ACC blues. "$brand-level" throttles the amount of color.   0 min-value  will not include and product color and 3 the max will bring in the most. 
+
+
+### Path and File Naming:
+_non-core/{type of style}/{product name}/{overide name or new UI name}.scss  
+
+#### Usage
+coming soon
+
+
+#### Products that Use the Design
+http://tools.acc.org/StatinIntolerance/#!/
+
+
+#### Style Guide w/ HTML Examples
+http://stage.tools.acc.org/BaseStyle/index.html
